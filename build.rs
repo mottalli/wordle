75ce93bb22d5220c4839c4d669f2505e20d1e9b9
Marcelo Mottalli <marcelo.mottalli@shopify.com>
2022-02-14 09:18:09 +0000

    Fixing build script

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     println!("cargo:rerun-if-changed=dictionaries/english/1.txt");
     Command::new("sh")
-        .args(&["-c", "./build-dictionary.py"])
+        .args(&["-c", "./build-dictionaries.sh"])
         .status()
         .unwrap();
 }
